8448372a98f29003c9d73243c7339c0c287845ea
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-09-02 12:45:21 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,10 @@
+n = int(input('Введите число от 3 до 20 '))
+number_key = []
+for i in range(1, n):
+    for j in range(i + 1, n):
+        if (n % (i + j)) == 0:
+            number_key.append(i)
+            number_key.append(j)
+        continue
+print(''.join(map(str, number_key)))
+
