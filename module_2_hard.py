991440b86670018e124954d9bc2a12fed210bfe3
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-09-02 12:45:01 +0300

    Delete module_2_hard.py

--- module_2_hard.py
+++
@@ -1,9 +0,0 @@
-n = int(input('Введите число от 3 до 20 '))
-number_key = []
-for i in range(1, n):
-    for j in range(i + 1, n):
-            if (n % (i + j)) == 0:
-                number_key.append(i, j)
-                number_key.append(j)
-            continue
-print(' '.join(map(str, number_key)))
