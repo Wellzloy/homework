147087aac75dc2d130cf46a273b49dc4207303cd
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-09-03 20:22:14 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -1,31 +1,14 @@
 def single_root_words(root_word='', *other_words ):
     same_words = []
-    print(root_word)
-    print(other_words)
     for i in other_words:
-        print(i)
-        if root_word in i:
-            print('Совпало')
+        if root_word.lower() in i.lower():
             same_words.append(i)
-    print(same_words)
     for i in other_words:
-        a = i.lower()
-        b = root_word.lower()
-        if a in b:
+        if i.lower() in root_word.lower():
             same_words.append(i)
-    print(same_words)
+    return same_words
 
-
-
-
-# result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-# same_words = []
-# text = 'Disablement'
-# other_words = ('Able', 'Mable', 'Disable', 'Bagel')
-# for i in other_words:
-#     a = i.lower()
-#     b = text.lower()
-#     if a in b:
-#         same_words.append(i)
-# print(same_words)
+print(result1)
+print(result2)
