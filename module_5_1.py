28f522714166011936e0e87be42fa9bab8dec8c3
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-09-15 22:55:31 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -3,7 +3,12 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def  go_to(self):
-        print('Проба')
+    def go_to(self, new_floor):
+        print(f'Номер нового этажа {new_floor}')
 
-h1 = House('ЖК Горский', 18)
+# h1 = House('ЖК Горский', 18)
+# Метод go_to выводит на экран(в консоль) значения от 1 до new_floor(включительно).
+# Если же new_floor больше чем self.number_of_floors или меньше 1, то вывести строку "Такого этажа не существует".
+
+well_ = House('Сосенки', 5)
+print(well_.name, well_.number_of_floors)
