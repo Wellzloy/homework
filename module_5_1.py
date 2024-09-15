a9efe6c89f67bae59673d7395dc41765f6ffd421
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-09-15 22:42:49 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,9 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def  go_to(self):
+        print('Проба')
+
+h1 = House('ЖК Горский', 18)
