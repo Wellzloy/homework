efd7f7d8cb613c6d802258b851464bc49b25680d
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-09-16 22:19:48 +0400

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,45 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if self.new_floor > self.number_of_floors or 1 > self.new_floor:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+# Задача "Магические здания":
+# Для решения этой задачи будем пользоваться решением к предыдущей задаче "Атрибуты и методы объекта".
+#
+# Необходимо дополнить класс House следующими специальными методами:
+#
+#     __len__(self) - должен возвращать кол-во этажей здания self.number_of_floors.
+#     __str__(self) - должен возвращать строку: "Название: <название>, кол-во этажей: <этажи>".
+#
+#
+# Пример результата выполнения программы:
+# Пример выполняемого кода:
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# # __str__
+# print(h1)
+# print(h2)
+#
+# # __len__
+# print(len(h1))
+# print(len(h2))
+#
+# Вывод на консоль:
+# Название: ЖК Эльбрус, кол-во этажей: 10
+# Название: ЖК Акация, кол-во этажей: 20
+# 10
+# 20
